988d80acf4fbf53fde459a21046e2e982046ace8
ConMark <connor.mccullough@cuanschutz.edu>
2020-10-11 23:03:27 -0600

    start of project

--- imagenoise.py
+++ imagenoise.py
@@ -1,3 +1,4 @@
+import tkinter as tk
 from tkinter import *
 from tkinter import filedialog
 from PIL import Image, ImageOps
@@ -8,57 +9,68 @@ import random
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib import pylab as plt
 
-root = Tk()
+class GUI(Frame):
+    def __init__(self,parent=None):
+        Frame.__init__(self,parent)
+        self.parent = parent
+        self.pack()
+        self.make_widgets()
 
-def browse():
-    root = Tk()
-    root.withdraw()
-    global file_path
-    file_path = filedialog.askopenfilename()
-    root.destroy()
+    def make_widgets(self):
+        self.winfo_toplevel().title("Image Noise")
 
-B = Button(root, text ="Select Image", command = browse)
-B.pack()
+        def browse():
+            root = tk.Tk()
+            root.withdraw()
+            global file_path
+            file_path = filedialog.askopenfilename()
+            root.destroy()
+        B = tk.Button(self, text ="Select Image", command = browse)
+        B.pack()
 
-randbut = IntVar()
-ijkbut = IntVar()
-rbut = Checkbutton(root, text = "random", variable = randbut, \
-        onvalue = 1, offvalue = 0, height=5, \
-        width = 20)
-ibut = Checkbutton(root, text = "i,j,k based", variable = ijkbut, \
-        onvalue = 1, offvalue = 0, height=5, \
-        width = 20)
-rbut.pack()
-ibut.pack()
+        proctype = IntVar()
+        randbut = tk.Radiobutton(root, 
+              text="Random",
+              padx = 20, 
+              variable=proctype, 
+              value=1)
+        ijkbut = tk.Radiobutton(root, 
+              text="I,J,K based",
+              padx = 20, 
+              variable=proctype, 
+              value=2)
+        randbut.pack()
+        ijkbut.pack()
 
-if randbut.get() == 1:
-    ijkbut.get() == 0
-if ijkbut.get() == 1:
-    randbut.get() == 0
+        def process():
+            im = Image.open(file_path)
+            im = im.resize((int(im.size[0]/10), int(im.size[1]/10)))
 
-def process():
-    im = Image.open(file_path)
-    im = im.resize((int(im.size[0]/10), int(im.size[1]/10)))
+            data = asarray(im)
+            pyplot.figure()
+            pyplot.imshow(data)
 
-    data = asarray(im)
-    pyplot.figure()
-    pyplot.imshow(data)
+            data2 = np.ndarray(shape=(im.size[1],im.size[0],3), dtype=float, order='F')
 
-    data2 = np.ndarray(shape=(im.size[1],im.size[0],3), dtype=float, order='F')
+            for i in range(0,im.size[1]):
+                for j in range(0,im.size[0]):
+                    for k in range(0,3):
+                        data2[i,j,k] = data[i,j,k] + random.randint(-30,30)
+                        if proctype.get() == 1:
+                            data2[i,j,k] = data[i,j,k] + random.randint(-30,30)
+                        if proctype.get() == 2:
+                            data2[i,j,k] = data[i,j,k] - i*2 + j*3 - k*4         
 
-    for i in range(0,im.size[1]):
-        for j in range(0,im.size[0]):
-            for k in range(0,3):
-                data2[i,j,k] = data[i,j,k] + random.randint(-30,30)
-                if randbut.get() == 1:
-                    data2[i,j,k] = data[i,j,k] + random.randint(-30,30)
-                if ijkbut.get() == 1:
-                    data2[i,j,k] = data[i,j,k] - i*2 + j*3 - k*4
-            
-    pyplot.figure()
-    pyplot.imshow(data2.astype('uint8'))
-    pyplot.show()
-P = Button(root, text ="Process", command = process)
-P.pack()
+            pyplot.figure()
+            pyplot.imshow(data2.astype('uint8'))
+            pyplot.show()
+        P = tk.Button(root, text ="Process", command = process)
+        P.pack()
 
+root = tk.Tk()
+gui = GUI(root)
 root.mainloop()
+
+
+
+
